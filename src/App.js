0d424a23411ba71e7547a6d49baa3d0367da4a3c
unknown <nishanthgowda018@gmail.com>
2021-07-14 00:13:55 -1200

    one

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import './App.css'
+import axios from 'axios';
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
+
+
+
+const App =()=>{
+    const [items, setItems] = useState([])
+    const [isLoading, setisLoading] = useState(true)
+    const [query, setQuery] = useState('')
+
+    useEffect(()=>{
+        const fetchItems = async ()=>{
+            const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+           // console.log(result.data)
+            setItems(result.data)
+            setisLoading(false)
+        }
+        fetchItems()
+    },[query])
+
+
+    return(
+        <div className='container'>
+        <Header/>
+        <Search getQuery={(q)=>setQuery(q)} />
+        <CharacterGrid isLoading={isLoading} items={items}/>
+        </div>
+    )
+}
+
+export default App;
